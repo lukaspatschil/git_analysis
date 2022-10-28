f1ff51db3d7f947ff1eeec4c6193a364b33c0c41
Lukas Spatschil <lukas@spatschil.at>
2022-10-28 18:16:38 +0200

    feat/add lit component library with storybook integration

---
+++ packages/git-visulizer/stories/participation.stories.ts
@@ -0,0 +1,8 @@
+import {html} from 'lit';
+import '../lib/participationChart';
+
+export default {
+    title: 'ParticipationChart'
+}
+
+export const Primary = () => html`<participation-chart></participation-chart>`;
