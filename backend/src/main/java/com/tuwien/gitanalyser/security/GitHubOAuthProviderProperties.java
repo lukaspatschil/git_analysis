42c2cd5818e0b7ce6dec3fa4c9a1b8e95383cca2
Simon Hofbauer <shofbauer98@gmail.com>
2022-11-19 13:46:19 +0100

    chore: delete secrets

--- backend/src/main/java/com/tuwien/gitanalyser/security/GitHubOAuthProviderProperties.java
+++ backend/src/main/java/com/tuwien/gitanalyser/security/GitHubOAuthProviderProperties.java
@@ -6,7 +6,7 @@ public class GitHubOAuthProviderProperties {
     public static final String CLIENT_NAME = "GitHub";
     public static final String REGISTRATION_ID = "github";
     public static final String CLIENT_ID = "abee8ba24898a4e6fd58";
-    public static final String CLIENT_SECRET = "958218d36a6a1c85f70b04f57b4bc05afffc861f";
+    public static final String CLIENT_SECRET = "";
     public static final String REDIRECT_URI = "http://localhost:8080/login/oauth2/code/github";
     public static final String AUTHORIZATION_URI = "https://github.com/login/oauth/authorize";
     public static final String TOKEN_URI = "https://github.com/login/oauth/access_token";
--- backend/src/main/java/com/tuwien/gitanalyser/security/GitLabOAuthProviderProperties.java
+++ backend/src/main/java/com/tuwien/gitanalyser/security/GitLabOAuthProviderProperties.java
@@ -6,7 +6,7 @@ public class GitLabOAuthProviderProperties {
     public static final String CLIENT_NAME = "GitLab";
     public static final String CLIENT_URL = "https://gitlab.com";
     public static final String CLIENT_ID = "b311e29e32f10daee03811fa223639fd597379efe20d86242551ad178950a29d";
-    public static final String CLIENT_SECRET = "9b2fe521fe0776c9cbdcae6229b8586d7a32fb2dd376613b2c9e09a5138ae5d9";
+    public static final String CLIENT_SECRET = "";
     public static final String REDIRECT_URI = "http://localhost:8080/login/oauth2/code/gitlab";
     public static final String[] SCOPES = {"read_user", "read_repository", "read_api", "api"};
     public static final String AUTHORIZATION_URI = "https://gitlab.com/oauth/authorize";
