feac5daf81bcc4a93eb7853e39fcecb2dcbf9e7a
Lukas Spatschil <lukas@spatschil.at>
2023-01-29 11:34:35 +0100

    feat: add login drop down

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Fragment, useEffect } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
-import { Link, useLocation } from 'react-router-dom';
+import { Bars3Icon, XMarkIcon, ChevronDownIcon } from '@heroicons/react/24/outline';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const user = {
   name: 'Tom Cook',
@@ -24,6 +24,7 @@ function classNames(...classes: any[]) {
 
 export default function Navbar() {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => navigation.forEach(path => path.href === pathname ? path.current = true : path.current = false), []);
 
@@ -97,6 +98,58 @@ export default function Navbar() {
                               )}
                             </Menu.Item>
                           ))}
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                    {/* Login drop down */}
+                    <Menu as="div" className="relative ml-3">
+                      <div>
+                        <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                          <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium flex">
+                            <span>Log in</span>
+                            <ChevronDownIcon
+                              className="ml-2 h-5 w-5"
+                              aria-hidden="true"
+                            />
+                          </span>
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Item>
+                            {({ active }) => (
+                                <Link
+                                to={{ pathname: "http://localhost:8080/oauth2/authorization/github" }}
+                                  className={classNames(
+                                    active ? 'bg-gray-100' : '',
+                                    'block px-4 py-2 text-sm text-gray-700'
+                                  )}
+                                >
+                                  Use GitHub
+                                </Link>
+                              )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                                <Link
+                                to={{ pathname: "http://localhost:8080/oauth2/authorization/gitlab" }}
+                                  className={classNames(
+                                    active ? 'bg-gray-100' : '',
+                                    'block px-4 py-2 text-sm text-gray-700'
+                                  )}
+                                >
+                                  Use GitLab
+                                </Link>
+                              )}
+                          </Menu.Item>
                         </Menu.Items>
                       </Transition>
                     </Menu>
