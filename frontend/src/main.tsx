2a9e9303999744f2dae04fa8e1c7c19fff3135fa
Lukas Spatschil <lukas@spatschil.at>
2023-01-29 10:38:10 +0100

    feat: add tailwind and react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import Home from './routes/home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
