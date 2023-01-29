db48a9957c43f70e444e7b08f93c6790ae98c42d
Simon Hofbauer <shofbauer98@gmail.com>
2023-01-29 15:26:41 +0100

    chore: move client secrets to environment variables

--- backend/src/main/java/com/tuwien/gitanalyser/security/AuthenticationConstants.java
+++ backend/src/main/java/com/tuwien/gitanalyser/security/AuthenticationConstants.java
@@ -15,7 +15,7 @@ public class AuthenticationConstants {
     public static final String GITHUB_CLIENT_NAME = "GitHub";
     public static final String GITHUB_REGISTRATION_ID = "github";
     public static final String GITHUB_CLIENT_ID = "abee8ba24898a4e6fd58";
-    public static final String GITHUB_CLIENT_SECRET = "";
+    public static final String GITHUB_CLIENT_SECRET = System.getenv("GITHUB_CLIENT_SECRET");
     public static final String GITHUB_REDIRECT_URI = "http://localhost:8080/login/oauth2/code/github";
     public static final String GITHUB_AUTHORIZATION_URI = "https://github.com/login/oauth/authorize";
     public static final String GITHUB_TOKEN_URI = "https://github.com/login/oauth/access_token";
@@ -24,7 +24,7 @@ public class AuthenticationConstants {
     public static final String GITLAB_CLIENT_NAME = "GitLab";
     public static final String GITLAB_CLIENT_URL = "https://gitlab.com";
     public static final String GITLAB_CLIENT_ID = "b311e29e32f10daee03811fa223639fd597379efe20d86242551ad178950a29d";
-    public static final String GITLAB_CLIENT_SECRET = "";
+    public static final String GITLAB_CLIENT_SECRET = System.getenv("GITLAB_CLIENT_SECRET");
     public static final String GITLAB_REDIRECT_URI = "http://localhost:8080/login/oauth2/code/gitlab";
     public static final String[] GITLAB_SCOPES = {"read_user", "read_repository", "read_api", "api"};
     public static final String GITLAB_AUTHORIZATION_URI = "https://gitlab.com/oauth/authorize";
