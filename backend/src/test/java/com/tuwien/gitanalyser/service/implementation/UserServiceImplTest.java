71c429b53c9c352ff2f0781b5db03752d0ca346c
Simon Hofbauer <shofbauer98@gmail.com>
2023-01-30 01:50:23 +0100

    feature: create user endpoint

--- backend/src/test/java/com/tuwien/gitanalyser/service/implementation/UserServiceImplTest.java
+++ backend/src/test/java/com/tuwien/gitanalyser/service/implementation/UserServiceImplTest.java
@@ -14,12 +14,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import utils.Randoms;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
@@ -30,9 +28,6 @@ class UserServiceImplTest extends ServiceImplementationBaseTest {
 
     private static final Long EXISTING_USER_ID = 1L;
     private static final Long NON_EXISTING_USER_ID = -1L;
-    private static final User USER_1 = mock(User.class);
-    private static final User USER_2 = mock(User.class);
-    private static final User USER_3 = mock(User.class);
     private UserService sut;
     private UserRepository userRepository;
 
@@ -73,8 +68,14 @@ class UserServiceImplTest extends ServiceImplementationBaseTest {
     @Test
     public void getById_existingId_returnsUser() throws NotFoundException {
         // Given
-        User expectedUser = new User(EXISTING_USER_ID, "John Doe", "john.doe@github.com", AuthenticationProvider.GITHUB,
-                                     Randoms.integer(), Randoms.alpha(), Randoms.alpha());
+        User expectedUser = new User(EXISTING_USER_ID,
+                                     "John Doe",
+                                     "john.doe@github.com",
+                                     Randoms.alpha(),
+                                     AuthenticationProvider.GITHUB,
+                                     Randoms.integer(),
+                                     Randoms.alpha(),
+                                     Randoms.alpha());
 
         when(userRepository.findById(EXISTING_USER_ID)).thenReturn(Optional.of(expectedUser));
 
@@ -95,33 +96,6 @@ class UserServiceImplTest extends ServiceImplementationBaseTest {
         assertThrows(NotFoundException.class, () -> sut.getUser(NON_EXISTING_USER_ID));
     }
 
-    @Test
-    public void getAll_noUsersInDB_returnsEmptyArray() {
-        // Given
-
-        when(userRepository.findAll()).thenReturn(new ArrayList<>());
-
-        // When
-        List<User> users = sut.getAll();
-
-        // Then
-        assertThat(users.size(), equalTo(0));
-    }
-
-    @Test
-    public void getAll_threeUsersInDB_returnsAllThreeUsers() {
-        // Given
-        List<User> allUsers = List.of(USER_1, USER_2, USER_3);
-
-        when(userRepository.findAll()).thenReturn(allUsers);
-
-        // When
-        List<User> users = sut.getAll();
-
-        // Then
-        assertThat(users, containsInAnyOrder(USER_1, USER_2, USER_3));
-    }
-
     @ParameterizedTest
     @EnumSource(AuthenticationProvider.class)
     public void processOAuthPostLogin_nonExistingUser_createsNewUser(AuthenticationProvider authenticationProvider) {
@@ -133,7 +107,7 @@ class UserServiceImplTest extends ServiceImplementationBaseTest {
         when(userRepository.findByPlatformAndPlatformId(authenticationProvider, platformId)).thenReturn(List.of());
 
         // When
-        sut.processOAuthPostLogin(createAuth2User(authenticationProvider, platformId, username),null);
+        sut.processOAuthPostLogin(createAuth2User(authenticationProvider, platformId, username), null);
 
         // Then
         verify(userRepository).save(expectedUser);
