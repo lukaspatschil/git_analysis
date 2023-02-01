8480a22d88cbd522294769925021c3187d497fe2
Lukas Spatschil <lukas@spatschil.at>
2023-02-01 18:34:40 +0100

    feat: add landing page and proper redirect on login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import Home from './routes/home';
 import Stats from './routes/stats';
 import Login from './routes/login';
+import Dashboard from './routes/dashboard';
+import Home from './routes/home';
 
 const router = createBrowserRouter([
   { path: '/', element: <Home /> },
+  { path: '/dashboard', element: <Dashboard /> },
   { path: '/stats', element: <Stats /> },
   { path: '/login', element: <Login /> }
 ]);
