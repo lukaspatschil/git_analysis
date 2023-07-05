40a3a14f68eb7f45ff0cfc73dd844df68795d6c7
Lukas Spatschil <lukas@spatschil.at>
2023-07-05 19:56:55 +0200

    feat: separate graphs to make it better visible

--- frontend/src/routes/branch/committerTimeline.tsx
+++ frontend/src/routes/branch/committerTimeline.tsx
@@ -83,7 +83,14 @@ export default function BranchOverview() {
                                 }
                             });
 
-                        return <Line options={committerOptions} data={committerDisplay} />;
+                        return <>
+                            <Line options={committerOptions} data={committerDisplay} />
+                            <div className="relative my-4">
+                                <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                                    <div className="w-full border-t border-gray-300"/>
+                                </div>
+                            </div>
+                        </>;
                     })}
                 </ul>
             </AsyncDataHandler>
